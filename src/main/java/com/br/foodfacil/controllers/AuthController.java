8e15aca9bfd9ecfc8b67d1edafcf6988f2434971
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-05-06 23:19:15 -0300

    origin vercel

--- src/main/java/com/br/foodfacil/controllers/AuthController.java
+++ src/main/java/com/br/foodfacil/controllers/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://food-facil-painel-admin-8mcqkbvbs-eliezerbrasilians-projects.vercel.app"})
 @RestController
 @RequestMapping(AppUtils.baseUrl + "/auth")
 public class AuthController {
