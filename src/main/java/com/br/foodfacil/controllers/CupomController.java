1c472772df9b5f150f7b835c8d469933c504d69c
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-05-14 21:05:22 -0300

    origin

--- src/main/java/com/br/foodfacil/controllers/CupomController.java
+++ src/main/java/com/br/foodfacil/controllers/CupomController.java
@@ -8,10 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @CrossOrigin(origins = {"http://localhost:5173",
         "https://food-facil-painel-admin-8mcqkbvbs-eliezerbrasilians-projects.vercel.app",
-        "https://food-facil-painel-admin.vercel.app," +
+        "https://food-facil-painel-admin.vercel.app" +
                 "https://foodfacil-website.vercel.app/"})
 @RestController
 @RequestMapping(AppUtils.baseUrl + "/cupom")
--- src/main/java/com/br/foodfacil/controllers/PedidosController.java
+++ src/main/java/com/br/foodfacil/controllers/PedidosController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = {"http://localhost:5173",
         "https://food-facil-painel-admin-8mcqkbvbs-eliezerbrasilians-projects.vercel.app",
-        "https://food-facil-painel-admin.vercel.app," +
-                "https://foodfacil-website.vercel.app"})
+        "https://food-facil-painel-admin.vercel.app", "https://foodfacil-website.vercel.app"})
 @RestController
 @RequestMapping(AppUtils.baseUrl + "/pedido")
 public class PedidosController {
